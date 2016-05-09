d9d27cde39d8c0cce09a4392cb4f559c37c7de9e
Czentye Bence Levente <lordlewo@gmail.com>
2016-05-09 10:53:38 +0200

    friendly-url-mapping

--- docroot/WEB-INF/src/hu/webtown/liferay/tvtracker/util/WebKeys.java
+++ docroot/WEB-INF/src/hu/webtown/liferay/tvtracker/util/WebKeys.java
@@ -2,10 +2,10 @@ package hu.webtown.liferay.tvtracker.util;
 
 public interface WebKeys extends com.liferay.portal.kernel.util.WebKeys {
 
-	public static final String TVSHOW_ID = "TVSHOW_ID";
+	public static final String TVSHOW_ID = "tvShowId";
 	
-	public static final String SEASON_ID = "SEASON_ID";
+	public static final String SEASON_ID = "seasonId";
 	
-	public static final String EPISODE_ID = "EPISODE_ID";
+	public static final String EPISODE_ID = "episodeId";
 	
 }
